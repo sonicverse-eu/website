4d9d09b272adac0dc623d44117e3c37b06f632b5
Rik Visser <rik@sonicverse.eu>
2026-04-13 15:07:28 +0200

    feat: migrate email handling to Resend API and add Prettier configuration

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -2,9 +2,7 @@ declare global {
 	interface CloudflareEnv {
 		ASSETS: Fetcher;
 		IMAGES: any;
-		IMAGE_ASSETS: R2Bucket;
-		SEND_EMAIL: any;
-		
+
 		// Add any additional bindings here
 		// Example:
 		// MY_KV: KVNamespace;
