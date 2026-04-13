ae7783ad6927cb9664f942c2346eb30c18652ed0
Rik Visser <rik@sonicverse.eu>
2026-04-13 21:19:02 +0200

    chore: add sanitize-preview-secrets script to remove preview mode keys from build artifacts

--- .open-next/server-functions/default/.next/server/middleware-build-manifest.js
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/03~yq9q893hmn.js"
   ],
   "lowPriorityFiles": [
-    "static/5xpAswidxeBfMButJ6iow/_buildManifest.js",
-    "static/5xpAswidxeBfMButJ6iow/_ssgManifest.js",
-    "static/5xpAswidxeBfMButJ6iow/_clientMiddlewareManifest.js"
+    "static/gBlGO7C-LZeNACLK05NKB/_buildManifest.js",
+    "static/gBlGO7C-LZeNACLK05NKB/_ssgManifest.js",
+    "static/gBlGO7C-LZeNACLK05NKB/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/05tsy9pxtuyv0.js",
